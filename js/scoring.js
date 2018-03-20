141afb6d6806bc4a8695b17677f99d149d910c1a
Jim Davies <jimfdavies@googlemail.com>
2018-03-20 00:06:04 +0000

    tidy up

--- js/scoring.js
+++ js/scoring.js
@@ -8,22 +8,16 @@ scalabilityAnswers['yes-automatic'] = 1
 scalabilityAnswers['yes-manual'] = 2
 scalabilityAnswers['no'] = 3
 
-function getRedundancyScore () {
+function calculateTotal () {
   var riskFactors = document.forms['riskfactors']
+
   var redundancyAnswer = riskFactors.elements['redundancy']
   var redundancyScore = redundancyAnswers[redundancyAnswer.value]
-  return redundancyScore
-}
 
-function getScalabilityScore () {
-  var riskFactors = document.forms['riskfactors']
   var scalabilityAnswer = riskFactors.elements['scalability']
   var scalabilityScore = scalabilityAnswers[scalabilityAnswer.value]
-  return scalabilityScore
-}
 
-function calculateTotal () {
-  var totalScore = getRedundancyScore() + getScalabilityScore()
+  var totalScore = redundancyScore + scalabilityScore
 
   var divobj = document.getElementById('totalScore')
   divobj.style.display = 'block'
